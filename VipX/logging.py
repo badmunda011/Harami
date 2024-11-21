a296cc261902a7ed94708eddc0d5772c0d6b3805
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-21 09:55:20 +0530

    Update logging.py

--- VipX/logging.py
+++ VipX/logging.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
