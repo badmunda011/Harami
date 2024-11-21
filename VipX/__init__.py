44b09a9394dbf2884c8976f715665d745edca6a4
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-11-21 09:58:09 +0530

    Update __init__.py

--- VipX/__init__.py
+++ VipX/__init__.py
@@ -3,30 +3,35 @@ from VipX.core.dir import dirr
 from VipX.core.git import git
 from VipX.core.userbot import Userbot
 from VipX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
 
 from .logging import LOGGER
 
-# Directories
+
 dirr()
 
-# Check Git Updates
 git()
 
-# Initialize Memory DB
 dbb()
 
-# Heroku APP
 heroku()
 
-# Load Sudo Users from DB
 sudo()
-# Bot Client
+
+# Clients
 app = VipXBot()
 
-# Assistant Client
 userbot = Userbot()
 
-from .platforms import PlaTForms
 
-Platform = PlaTForms()
-HELPABLE = {}
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
